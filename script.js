c5cd095421946c0ed2e8bb34f105b4074c40c8e5
stocksociety <arnold.kagunda@student.moringaschool.com>
2021-11-20 08:01:01 +0300

    save the changes

--- script.js
+++ script.js
@@ -1,40 +1,34 @@
-// form area to fill in the user's data and make it responsive
-// $(document).ready(function(){
-//     $("#development-image").click(function(){
-//       $("#development-image").slideDown('1500').hide('1000');
-//       $("#development").show('1500');
-//     });
-//     $("#development").click(function(){
-//       $("#development").slideUp('1500');
-//       $("#development-image").slideDown('1500');
-//     });
-//   });
-  
-//   $(document).ready(function(){
-//     $("#design-image").click(function(){
-//       $("#design-image").slideDown('1500').hide('1000');
-//       $("#design").show('1500');
-//     });
-//     $("#design").click(function(){
-//       $("#design").slideUp('1500');
-//       $("#design-image").slideDown('1500');
-//     });
-//   });
-  
-//   $(document).ready(function(){
-//     $("#product-image").click(function(){
-//       $("#product-image").slideDown('1500').hide('1000');
-//       $("#product").show('1500');
-//     });
-//     $("#product").click(function(){
-//       $("#product").slideUp('1500');
-//       $("#product-image").slideDown('1500');
-//     });
-//   });
-// $(document).ready(function(){
-// $(".design").click(function()
-// $("design").hide(1200))
-// $(".design").toggle(1200)
-// $(".design").show(1200)
 
-// })
+$(document).ready(function(){
+    $("#design").click(function(){
+      $("#design").slideDown('1500').hide('1000');
+      $("#design").show('1500');
+    });
+    $("#design").click(function(){
+      $("#design").slideUp('1500');
+      $("#design").slideDown('1500');
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#design-image").click(function(){
+      $("#design-image").slideDown('1500').hide('1000');
+      $("#design").show('1500');
+    });
+    $("#design").click(function(){
+      $("#design").slideUp('1500');
+      $("#design-image").slideDown('1500');
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#product-image").click(function(){
+      $("#product-image").slideDown('1500').hide('1000');
+      $("#product").show('1500');
+    });
+    $("#product").click(function(){
+      $("#product").slideUp('1500');
+      $("#product-image").slideDown('1500');
+    });
+  });
+
